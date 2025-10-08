c09e969ba2cdef2e688c7c2e8580cb1731b06f02
kevintram <thekevintram@gmail.com>
2025-10-08 16:45:33 -0400

    feat(core): load db settings from env

--- app/core/db.py
+++ app/core/db.py
@@ -1,23 +1,13 @@
-from pydantic import PostgresDsn
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine
 import asyncio
-
-POSTGRES_URL = PostgresDsn.build(
-    scheme="postgresql+asyncpg",
-    username="postgres",
-    password="postgres",
-    host="127.0.0.1",
-    port=54322, 
-    path="postgres",
-)
+from .config import settings
 
 
 async def main():
-    print(str(POSTGRES_URL))
+    print(str(settings.POSTGRES_URL))
 
-
-    engine = create_async_engine(str(POSTGRES_URL), echo=True)
+    engine = create_async_engine(str(settings.POSTGRES_URL), echo=True)
 
     async with engine.begin() as conn:
         result = await conn.execute(text("select * from topics"))
