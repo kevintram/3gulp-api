9f9aca7452a3a3553b2cb592dd927213c00bd510
kevintram <thekevintram@gmail.com>
2025-10-14 17:08:05 -0400

    feat: adds auth endpoints
    - uses authkit

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from .dependencies import get_token_header
 from .internal import admin
-from .routers import items, users, topics
+from .routers import items, users, topics, login
 from .core.config import settings
 
 app = FastAPI()
@@ -19,6 +19,7 @@ app.add_middleware(
 app.include_router(users.router)
 app.include_router(items.router)
 app.include_router(topics.router)
+app.include_router(login.router)
 app.include_router(
     admin.router,
     prefix="/admin",
